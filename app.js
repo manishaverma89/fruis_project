58946992c14f8280de51adae2ec0786455d67d98
manisha <manisha.verma23@gmail.com>
2022-05-01 07:27:27 +0530

    Created document using mongoose

--- app.js
+++ app.js
@@ -1,20 +1,62 @@
-const MongoClient = require("mongodb").MongoClient;
-const assert = require("assert");
-
-// connection url
-const url ="mongodb://localhost:27017";
-
-// Database Name
-const dbName = "fruitsDB";
-
-// Create a new MongoClient
-const client = new MongoClient(url);
-
-// use connect method to connect to the server
-client.connect(function(err){
- assert.equal(null, err);
- console.log("connected successfully to server");
- const db = client.db(dbName);
- client.close();
-
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/fruitsDB", {
+  useNewUrlParser: true,
 });
+
+//Create a new schema
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String,
+});
+
+//Create mongoose model
+const Fruit = new mongoose.model("Fruit", fruitSchema); //Now we're ready to create a fruit document
+
+const apple = new Fruit({
+    name: "Apple",
+    rating: 8,
+    review: "A great fruit"
+});
+//fruit.save();    //if want to save 1 fruit
+
+//To save many fruits, use Fruit.insertmany() function
+const kiwi = new Fruit({
+name: "Kiwi",
+rating: 9,
+review: "must have food"
+});
+
+const grapes = new Fruit({
+    name: "Grapes",
+    rating: 7,
+    review: "good food"
+    });
+
+
+Fruit.insertMany([apple, kiwi, grapes], function(err){
+    if(err){
+        console.log(err);
+     }
+     else{
+         console.log("successfully saved all the fruits to fruitDB");
+     }
+
+}); 
+
+// create a new schema for a Person
+ const personSchema = new mongoose.Schema({
+     name: String,
+     age: Number
+ });
+
+// create a mongoose model
+const Person = new mongoose.model("Person", personSchema);
+
+const person = new Person({
+    name: "John",
+    age: 37
+});
+person.save();
+
+
